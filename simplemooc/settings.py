301691857bbbca91cd76993abd8ff30148a1739b
Pedro Pinho <pedropinho37@gmail.com>
2019-04-11 16:15:52 -0300

    Alteração visando eliminar o bug do css

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -164,7 +164,7 @@ DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/core/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
