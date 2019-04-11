ff10be1fca988d8e62146f786bc601ccc6f4d96c
Pedro Pinho <pedropinho37@gmail.com>
2019-04-11 18:03:52 -0300

    Agora vai 4

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -168,7 +168,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'simplemooc/static'),
+    os.path.join(BASE_DIR, 'simplemooc/core/static'),
 )
 
 import dj_database_url
