3bdfdd9c83848413aeb7bf77b49983d3471987cd
Pedro Pinho <pedropinho37@gmail.com>
2019-04-11 18:49:02 -0300

    Pasta de arquivos estaticos agora se encontra na raiz do projeto

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -162,15 +162,6 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'app/staticfiles'),
-)
-
 import dj_database_url
 try:
     from simplemooc.local_settings import *
