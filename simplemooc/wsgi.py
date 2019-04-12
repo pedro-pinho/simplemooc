8dd27562c55766ce2915d0ef57821be56b50d5a1
Pedro Pinho <pedropinho37@gmail.com>
2019-04-12 09:22:47 -0300

    conf cling

--- simplemooc/wsgi.py
+++ simplemooc/wsgi.py
@@ -14,5 +14,5 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 
-application = get_wsgi_application()
-# application = Cling(get_wsgi_application())
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
